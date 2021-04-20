0345291fc363d66fb8408fe1b4c970731b3e630e
viper <liutsungcheng@gmail.com>
2021-04-20 13:23:45 +0800

    嚶嚶嚶

--- 2021-04-19/server.js
+++ 2021-04-19/server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const {readFile} = require('./fsUtil')
+const { readFile } = require('./fsUtil')
 
 let fileName = './datas.json'
 
 //app.use(express.static('web'));
 
-app.get('/',async (req,res)=>{
+app.get('getUserList', async (req, res) => {
 
-    res.sendFile( __dirname + "/" + "userList.html" );
+    //回應資料給網頁
+    // 處理請求參數: pageIndex, pageSize
+
+    /*
+        let data = await readFile(fileName)
+        let dataJSON = JSON.parse(data)
+
+        console.log(dataJSON[0]);
+    */
+
+});
+
+app.get('/', async (req, res) => {
+
+    res.sendFile(__dirname + "/" + "userList.html");
     console.log(req.path);
-    let data = await readFile(fileName)
-    let dataJSON = JSON.parse(data)
-    
-    console.log(dataJSON[0]);
-    
+
+
 });
 
 
