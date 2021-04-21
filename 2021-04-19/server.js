f339128a07374bad15ceca29454baafd05b542b8
chih-ying123 <g26350156@gmail.com>
2021-04-21 17:56:59 +0800

    寫出了神奇詭異的東西

--- 2021-04-19/server.js
+++ 2021-04-19/server.js
@@ -11,14 +11,16 @@ app.get('/getUserList', async (req, res) => {
 
     let pageIndex = parseInt(req.query.pageIndex, 10); //pageIndex、 pageSize 字串
     let pageSize = parseInt(req.query.pageSize, 10);  // 要轉成int，不然四則運算可能會錯
+    let listSize = 5;
 
     //console.log(typeof pageIndex);
     //console.log(typeof pageSize);
-    
-    let  userList = await getUserList(pageIndex, pageSize);
-    let totalpage = await totalPage(pageSize);
-    let resDatas =[userList, totalpage]
-    //console.log(resDatas);
+
+    let datasInfo = await getUserList(pageIndex, pageSize);
+    let totalRows = datasInfo.totalRows;
+    let pageInfo = await getPagination(totalRows, pageSize, pageIndex, listSize)
+
+    let resDatas = {datasInfo, pageInfo};
 
     res.json(resDatas);
 });
@@ -30,6 +32,8 @@ app.get('/', async (req, res) => {
 async function getUserList(pageIndex, pageSize) {
     let data = await readFile(fileName); //字串
     let dataJSON = JSON.parse(data);
+    let totalRows = dataJSON.length;
+    let totalpage = Math.ceil(totalRows/pageSize) ;
 
     let startRow = pageSize * (pageIndex - 1);
     let endRow = (pageSize - 1) + pageSize * (pageIndex - 1);
@@ -37,29 +41,71 @@ async function getUserList(pageIndex, pageSize) {
     if (startRow < 0){
         startRow = 0;
     }
-
-    if (endRow >= dataJSON.length){
-        endRow = dataJSON.length-1;
+    if (endRow >= totalRows){
+        endRow = totalRows-1;
     }
 
-    let data2 = [];
-
+    let userList = [];
     for (var i = startRow; i <= endRow; i++) {
-        data2.push(dataJSON[i]);
+        userList.push(dataJSON[i]);
     }
-    return data2;
+
+    return {userList, totalRows};
 }
 
-async function totalPage(pageSize){
 
-    let data = await readFile(fileName); //字串
-    let dataJSON = JSON.parse(data);
+function getPagination(totalRows, pageSize, currentPage, listSize) {
 
-    let totalRows = dataJSON.length;
+    let totalPages = 0; //總頁數
+    let startPage = 0;
+    let endPage = 0;
 
-    let totalpage = Math.ceil(totalRows/pageSize) ;
-    return totalpage;
 
+    //計算總頁數
+    if (totalRows === 0) {
+        totalPages = 1;
+    } else if (totalRows % pageSize == 0) {
+        totalPages = totalRows / pageSize;
+    } else {
+        /*取地板值*/
+        totalPages = Math.floor(totalRows / pageSize) + 1; //Math.floor() 回傳小於等於所給數字的最大整數
+    }
+
+    //防呆: 當前頁次超出總頁數
+    if (currentPage > totalPages) {
+        currentPage = totalPages;
+    }
+
+    if (totalPages <= listSize) {
+        startPage = 1;
+        endPage = totalPages;
+    } else {
+
+        startPage = currentPage - (listSize / 2) + 1;
+        endPage = currentPage + (listSize / 2);
+
+        if (startPage < 1) {
+            startPage = 1;
+            endPage = listSize;
+        }
+
+        if (endPage > totalPages) {
+            endPage = totalPages;
+            startPage = totalPages - (listSize - 1);
+        }
+    }
+
+    let prePage = currentPage - 1 <= 0 ? 1 : currentPage - 1;
+    let nextPage = currentPage + 1 > totalPages ? totalPages : currentPage + 1;
+
+    return {
+        currentPage
+        , startPage
+        , endPage
+        , prePage
+        , nextPage
+    };
+    
 }
 
 app.listen(3000);
