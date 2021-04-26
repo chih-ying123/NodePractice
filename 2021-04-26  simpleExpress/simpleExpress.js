4e2bcb7a079256d95960e4f62f2771730aee3baa
viper <liutsungcheng@gmail.com>
2021-04-26 15:32:49 +0800

    .

--- 2021-04-26  simpleExpress/simpleExpress.js
+++ 2021-04-26  simpleExpress/simpleExpress.js
@@ -37,46 +37,6 @@ function express() {
 module.exports = express;
 
 
-
-/*
-
-function express() {
-
-    let app = function (req, res) {
-
-        let reqPath = url.parse(req.url).pathname;
-        let reqMethod = req.method.toLowerCase();
-
-        for (let i = 0; i < app.routes.length; i++) {
-            let route = app.routes[i];
-            if (route.method === reqMethod && route.path === reqPath) {
-                route.handler(req, res);
-            }
-        }
-        return res.end(`can not ${reqMethod} ${reqPath}`);
-
-    };
-
-    app.routes = [];
-    app.get = function (path, handler) {
-        app.routes.push({
-            method: 'get'
-            , path
-            , handler
-        })
-    }
-
-    app.listen = function () {
-        let server = http.createServer(app);
-        server.listen(...arguments);
-    }
-
-    return app;
-}
-*/
-
-
-
 /*
 
 	http.createServer(app);
