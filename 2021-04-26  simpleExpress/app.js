d3cfcd5c1d37df9fd9c15ed46cf41ea1b1631272
viper <liutsungcheng@gmail.com>
2021-04-26 15:30:38 +0800

    嚶嚶嚶

---
+++ 2021-04-26  simpleExpress/app.js
@@ -0,0 +1,24 @@
+/*
+    express 只是對原生的http做出功能封裝，方便使用。
+*/
+
+const express = require('./simpleExpress');
+let app = express();   //express 是一個函式 (一對圓括號調用) 
+
+
+// 註冊一個路由: 匹配規則 
+app.get('/a', function (req, res) {
+    res.end('a');
+});
+
+app.get('/b', function (req, res) {
+    res.end('b');
+});
+
+app.get('/c', function (req, res) {
+    res.end('c');
+});
+
+app.listen(3000, function () {
+    console.log('Server start at 3000 port');
+});
---
+++ 2021-04-26  simpleExpress/simpleExpress.js
@@ -0,0 +1,98 @@
+const http = require('http');
+const url = require('url');
+
+function express() {
+
+    let app = function (req, res) {
+
+        let reqPath = url.parse(req.url).pathname;
+        let reqMethod = req.method.toLowerCase();
+
+        for (let i = 0; i < app.routes.length; i++) {
+            let route = app.routes[i];
+            if (route.method === reqMethod && route.path === reqPath) {
+                route.handler(req, res);
+            }
+        }
+        return res.end(`can not ${reqMethod} ${reqPath}`);
+    }
+
+    app.routes = [];
+    app.get = function (path, handler) {
+        app.routes.push({
+            method: 'get'
+            , path
+            , handler
+        }) 
+    }    
+
+    app.listen = function () {
+        let server = http.createServer(app);
+        server.listen(...arguments);  // 3000, fn
+    }
+
+    return app;    
+}
+
+module.exports = express;
+
+
+
+/*
+
+function express() {
+
+    let app = function (req, res) {
+
+        let reqPath = url.parse(req.url).pathname;
+        let reqMethod = req.method.toLowerCase();
+
+        for (let i = 0; i < app.routes.length; i++) {
+            let route = app.routes[i];
+            if (route.method === reqMethod && route.path === reqPath) {
+                route.handler(req, res);
+            }
+        }
+        return res.end(`can not ${reqMethod} ${reqPath}`);
+
+    };
+
+    app.routes = [];
+    app.get = function (path, handler) {
+        app.routes.push({
+            method: 'get'
+            , path
+            , handler
+        })
+    }
+
+    app.listen = function () {
+        let server = http.createServer(app);
+        server.listen(...arguments);
+    }
+
+    return app;
+}
+*/
+
+
+
+/*
+
+	http.createServer(app);
+
+	=> 內部實現偽代碼(示意用， 不是真正的實現)
+
+		http.createServer = function(callBack){
+
+			let request  = 解析http請求文本 ... {請求headers、請求路徑、請求方法}
+			let response = ....
+
+			http.on('request', function(){  // 當 `每一次` 請求到來的時候調用
+
+				callBack(request, response)
+			
+			}) ;
+		}
+
+*/
