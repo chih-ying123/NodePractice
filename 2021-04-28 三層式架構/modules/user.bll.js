d6cdec72896975b3e5c1ca3709674518b43dac19
viper <liutsungcheng@gmail.com>
2021-04-28 13:26:50 +0800

    .

--- 2021-04-28 三層式架構/modules/user.bll.js
+++ 2021-04-28 三層式架構/modules/user.bll.js
@@ -1,23 +1,20 @@
-const { getPagination } = require('../common');
-const dal = require('./user.dal'); // 這一層負責跟資料庫溝通
+const { resultMessage, getPagination } = require('../common');
+const dal = require('./user.dal')
 
 
 async function getUserList(pageIndex, pageSize) {
 
     let result = await dal.getUserList(pageIndex, pageSize);
-
-    // getPagination(totalRows, pageSize, currentPage, listSize)
     let pageInfo = getPagination(result.totalRows, pageSize, pageIndex, 10);
-    return {
+
+    return resultMessage(0, '', {
         datas: result.datas
         , pageInfo
-    }
-
+    });
 }
 
 
 module.exports = {
     getUserList
-
 
 }
