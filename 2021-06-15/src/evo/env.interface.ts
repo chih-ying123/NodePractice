27b1f5ff2b45e5b3834e8376a3ca476a89c1b9d9
chih-ying123 <g26350156@gmail.com>
2021-06-18 13:11:56 +0800

    任性的nest

--- 2021-06-15/src/evo/env.interface.ts
+++ 2021-06-15/src/evo/env.interface.ts
@@ -1,13 +1,29 @@
 export interface IEnv {
-
     baseURI: string;
     historyAPI: string;
     casinoId: string;
     gameHistoryApiToken: string;
-
 }
 
 export interface APIResponse{
-    uuid:string
-    uuid2:string
+    uuid: string;
+    data: data[];
+}
+
+export interface data{
+    date: string;
+    games: games[];
+}
+
+export interface games{
+    id: string;
+    startedAt: string;
+    settledAt: string;
+    status: string;
+    gameType: string;
+    participants: participants[];
+}
+
+export interface participants{
+    playerId: string;
 }
