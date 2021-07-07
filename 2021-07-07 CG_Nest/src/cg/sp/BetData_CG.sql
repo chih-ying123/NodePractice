04f0bdb3553c3be9aa71c0409beef01f6d9f801b
TC <liutsungcheng@gmail.com>
2021-07-07 15:27:21 +0800

    .

--- 2021-07-07 CG_Nest/src/cg/sp/BetData_CG.sql
+++ 2021-07-07 CG_Nest/src/cg/sp/BetData_CG.sql
@@ -1,12 +1,18 @@
-CREATE TABLE `BetData_CG`(
-    `SerialNumber`        VARCHAR(32) 
-  , `GameType` VARCHAR(32) DEFAULT NULL
-  , `LogTime` DATETIME DEFAULT NULL
-  , `ThirdPartyAccount` VARCHAR(32) DEFAULT NULL
-  , `ValidBet` DECIMAL(14, 4) DEFAULT NULL
-  , `MoneyWin` DECIMAL(14, 4) DEFAULT NULL
-  , `WinLose` DECIMAL(14, 4) DEFAULT NULL
-  , `NormalWin` DECIMAL(14, 4) DEFAULT NULL
-  , `BonusWin` DECIMAL(14, 4) DEFAULT NULL
-  , PRIMARY KEY(SerialNumber) 
-) ENGINE=INNODB DEFAULT CHARSET=utf8mb4
+DROP TABLE IF EXISTS BetData_CG ;
+
+CREATE TABLE BetData_CG(
+   SerialNumber                 BIGINT UNSIGNED NOT NULL
+   , GameType                   VARCHAR(32)     NOT NULL
+   , LogTime                    DATETIME        NOT NULL
+   , BetMoney                   DECIMAL(12, 2)  NOT NULL
+   , MoneyWin                   DECIMAL(12, 2)  NOT NULL
+   , NormalWin                  DECIMAL(12, 2)  NOT NULL
+   , BonusWin                   DECIMAL(12, 2)  NOT NULL
+   , JackpotMoney               DECIMAL(12, 2)  NOT NULL
+   , ThirdPartyAccount          VARCHAR(50)     NOT NULL
+   , ValidBet                   DECIMAL(12, 2)  NOT NULL
+   , Device                     VARCHAR(50)     NOT NULL
+   , IPAddress                  VARCHAR(50)     NOT NULL
+   , WinLose                    DECIMAL(12, 2)  NOT NULL  -- MoneyWin - ValidBet
+   , PRIMARY KEY(SerialNumber) 
+);
